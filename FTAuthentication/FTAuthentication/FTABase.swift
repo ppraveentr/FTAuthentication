3fee910a7f62d48a51ba9fe59f7ac3152c3f02d9
Praveen P <ppraveentr@gmail.com>
2017-08-05 13:56:45 +0530

    Initial commit

---
+++ FTAuthentication/FTAuthentication/FTABase.swift
@@ -0,0 +1,15 @@
+//
+//  FTABase.swift
+//  FTAuthentication
+//
+//  Created by Praveen Prabhakar on 05/08/17.
+//  Copyright © 2017 Praveen Prabhakar. All rights reserved.
+//
+
+import Foundation
+
+public class FTBase {
+    public class func logToConsole(msg: String) {
+        print(msg)
+    }
+}
